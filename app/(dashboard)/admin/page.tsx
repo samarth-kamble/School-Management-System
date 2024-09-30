49ac65fb43249648aab49f137c73e54a1324f48c
Samarth Kamble <kamblesamarth04@gmail.com>
2024-09-30 18:57:07 +0530

    Implement the user card and count chart

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
-    return (
-        <div>AdminPage</div>
-    )
-}
-export default AdminPage
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* User Card */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type={"student"} />
+          <UserCard type={"teacher"} />
+          <UserCard type={"parent"} />
+          <UserCard type={"staff"} />
+        </div>
+        {/* Middle Chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+        </div>
+      </div>
+      {/* RIGHT  */}
+      <div className="w-full lg:w-1/3">L</div>
+    </div>
+  );
+};
+export default AdminPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,15 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        lamaSky: "#C3EBFA",
+        lamaSkyLight: "#EDF9FD",
+        lamaPurple: "#CFCEFF",
+        lamaPurpleLight: "#F1F0FF",
+        lamaYellow: "#FAE27C",
+        lamaYellowLight: "#FEFCE8",
       },
     },
   },
