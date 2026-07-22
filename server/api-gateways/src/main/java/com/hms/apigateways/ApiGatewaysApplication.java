2a3e4d76b7b96b403e55b8d135830231e1b9ca7d
Samarth Kamble <kamblesamarth04@gmail.com>
2026-07-22 23:14:28 +0530

    Setting up required services

---
+++ server/api-gateways/src/main/java/com/hms/apigateways/ApiGatewaysApplication.java
@@ -0,0 +1,13 @@
+package com.hms.apigateways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewaysApplication.class, args);
+	}
+
+}
---
+++ server/pharmacy-service/src/main/java/com/hms/pharmacyservice/PharmacyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hms.pharmacyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyServiceApplication.class, args);
+	}
+
+}
